229d3bb1d0b0bbc9472e4fce4a5bdcc442967943
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-27 16:30:54 +0900

    initial commit

---
+++ lib/pelemay_fp_benchmark.ex
@@ -0,0 +1,18 @@
+defmodule PelemayFpBenchmark do
+  @moduledoc """
+  Documentation for `PelemayFpBenchmark`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PelemayFpBenchmark.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pelemay_fp_benchmark_test.exs
@@ -0,0 +1,8 @@
+defmodule PelemayFpBenchmarkTest do
+  use ExUnit.Case
+  doctest PelemayFpBenchmark
+
+  test "greets the world" do
+    assert PelemayFpBenchmark.hello() == :world
+  end
+end
