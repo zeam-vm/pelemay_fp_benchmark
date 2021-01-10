5caa83e22fe32a8a1e1e3bb74ba8c56fc6668cc6
Susumu Yamazaki <zacky1972@gmail.com>
2021-01-10 09:41:37 +0900

    add test Pelemay and PelemayFp

--- test/pelemay_fp_benchmark_test.exs
+++ test/pelemay_fp_benchmark_test.exs
@@ -5,4 +5,8 @@ defmodule PelemayFpBenchmarkTest do
   test "greets the world" do
     assert PelemayFpBenchmark.hello() == :world
   end
+
+  test "test PelemayFp and Pelemay" do
+    assert Enum.map(1..100000, &LogisticMap.logistic_map_10(&1)) == PelemayFp.map_chunk(1..100000, &LogisticMap.logistic_map_10(&1), &LogisticMap.logistic_map_10_pelemay(&1))
+  end
 end
